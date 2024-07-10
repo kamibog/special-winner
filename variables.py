10a235c81b2e79f433d2b6a5bf9fbae98c7dedca
kamibog <kameneckaya@mail.ru>
2024-07-10 11:21:23 +0700

    Add files via upload

---
+++ variables.py
@@ -0,0 +1,9 @@
+number_of_completed_tasks='12'
+print(number_of_completed_tasks)
+number_of_hours_spent='1.5'
+print(number_of_hours_spent)
+course_name='Python'
+print(course_name)
+time_for_one_task=(1.5/12)
+print(time_for_one_task)
+print('курс', course_name,'всего задач', number_of_completed_tasks, 'затрачено часов', number_of_hours_spent, 'среднее время выполнения', time_for_one_task )
