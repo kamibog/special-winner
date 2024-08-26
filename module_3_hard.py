78ef779c78a4164f81bdf9c5d48fe630f5273e42
kamibog <kameneckaya@mail.ru>
2024-08-26 16:11:10 +0700

    Add files via upload

--- module_3_hard.py
+++ module_3_hard.py
@@ -1,20 +1,29 @@
-def calculate_structure_sum(data):
-    total = 0
+def calculate_structure_sum(data_structure):
+    sum_numbers = 0
+    sum_strings = 0
 
-    for element in data:
-        if isinstance(element, int) or isinstance(element, float):
-            total += element
-        elif isinstance(element, str):
-            total += len(element)
-        elif isinstance(element, list) or isinstance(element, tuple):
-            total += calculate_structure_sum(element)
-        elif isinstance(element, dict):
-            for key, value in element.items():
-                total += calculate_structure_sum([key])
-                total += calculate_structure_sum([value])
-    return total
+    def recurse(data):
+        nonlocal sum_numbers, sum_strings
+        if isinstance(data, list) or isinstance(data, tuple) or isinstance(data, set):
+            for item in data:
+                recurse(item)
+        elif isinstance(data, dict):
+            for value in data.values():
+                recurse(value)
+            for key in data.keys():
+                recurse(key)
+        elif isinstance(data, int) or isinstance(data, str):
+            if isinstance(data, int):
+                sum_numbers += data
+            elif isinstance(data, str):
+                sum_strings += len(data)
 
-data_structure = [[1, 2, 3],{'a': 4, 'b': 5},(6, {'cube': 7, 'drum': 8}),"Hello",((), [{(2, 'Urban', ('Urban2', 35))}])]
+    recurse(data_structure)
+    return sum_numbers + sum_strings
+
+
+data_structure = [[1, 2, 3], {'a': 4, 'b': 5}, (6, {'cube': 7, 'drum': 8}), "Hello",
+                  ((), [{(2, 'Urban', ('Urban2', 35))}])]
 
 result = calculate_structure_sum(data_structure)
 print(result)
