bd56d58db3b509538496c5f9fe4aa01009e24838
kamibog <kameneckaya@mail.ru>
2024-09-09 13:49:20 +0700

    Add files via upload

---
+++ module_7_3.py
@@ -0,0 +1,39 @@
+import string
+
+
+class WordsFinder:
+    def __init__(self, *file_names):
+        self.file_names = file_names
+
+    def get_all_words(self):
+        all_words = {}
+        for file_name in self.file_names:
+            with open(file_name, 'r', encoding='utf-8') as file:
+                text = file.read().lower()
+                text = text.translate(str.maketrans('', '', string.punctuation.replace('-', '')))
+                words = text.split()
+                all_words[file_name] = words
+        return all_words
+
+    def find(self, word):
+        word = word.lower()
+        result = {}
+        all_words = self.get_all_words()
+        for file_name, words in all_words.items():
+            if word in words:
+                result[file_name] = words.index(word)
+        return result
+
+    def count(self, word):
+        word = word.lower()
+        result = {}
+        all_words = self.get_all_words()
+        for file_name, words in all_words.items():
+            result[file_name] = words.count(word)
+        return result
+
+
+finder2 = WordsFinder('test_WordFinder.txt')
+print(finder2.get_all_words())
+print(finder2.find('TEXT'))
+print(finder2.count('teXT'))
