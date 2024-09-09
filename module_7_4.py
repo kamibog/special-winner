41252c2fd35a22bdab224988d752dc9f15859dcb
kamibog <kameneckaya@mail.ru>
2024-09-09 15:01:40 +0700

    Add files via upload

---
+++ module_7_4.py
@@ -0,0 +1,16 @@
+team1_num = 5
+print('В команде Мастера участников: %s' % team1_num)
+team2_num = 6
+print('Итого сегодня участников: %s и %s' % (team1_num, team2_num))
+score_1 = 40
+score_2 = 42
+team1_time = 1552.512
+team2_time = 2153.31451
+print('Команда Волшебники данных решила задач:{}'.format(score_2))
+print("Волшебники данных решили задачи за {} с !".format(team2_time))
+tasks_total = 82
+time_avg = 45.2
+print(f'Команды решили {score_1} и {score_2} задач')
+challenge_result = 'Победа команды Мастера кода!'
+print(f'Результат битвы: {challenge_result}')
+print(f'Сегодня было решено {tasks_total} задач, в среднем по {time_avg} секунды на задачу!.')
