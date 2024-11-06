6b2ef2f91f034c403eef3554c07c3da71bad21f0
kamibog <kameneckaya@mail.ru>
2024-11-06 11:31:48 +0700

    Add files via upload

--- module_13_1.py
+++ module_13_1.py
@@ -13,11 +13,12 @@ async def start_strongman(name, power):
 
 async def start_tournament():
     task = []
-    task.append(start_strongman('Pasha', 3))
-    task.append(start_strongman('Denis', 4))
-    task.append(start_strongman('Apollon', 5))
-
-    await asyncio.gather(*task)
+    task1 = asyncio.create_task(start_strongman('Pasha', 3))
+    task2 = asyncio.create_task(start_strongman('Denis', 4))
+    task3 = asyncio.create_task(start_strongman('Apollon', 5))
+    await task1
+    await task2
+    await task3
 
 
 asyncio.run(start_tournament())
