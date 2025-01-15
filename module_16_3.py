78859dbeb23f18980ce1afba300eb1f1edbeced1
kamibog <kameneckaya@mail.ru>
2025-01-15 11:05:20 +0700

    Add files via upload

--- module_16_3.py
+++ module_16_3.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI, Path
+from typing import Annotated
 
 app = FastAPI()
 
@@ -9,21 +10,31 @@ async def get_users() -> dict:
     return users
 
 @app.post("/user/{username}/{age}")
-async def post_user(username: str, age: int) -> str:
+async def post_user(
+    username: Annotated[str, Path(min_length=5, max_length=20, description="Enter username", example="UrbanUser")],
+    age: Annotated[int, Path(ge=18, le=120, description="Enter age", example=24)]
+) -> str:
     new_id = str(int(max(users.keys())) + 1)
     users[new_id] = f"Имя: {username}, возраст: {age}"
     return f"User {new_id} is registered"
 
 @app.put("/user/{user_id}/{username}/{age}")
-async def update_user(user_id: str, username: str, age: int) -> str:
+async def update_user(
+    user_id: Annotated[str, Path(description="Enter User ID")],
+    username: Annotated[str, Path(min_length=5, max_length=20, description="Enter username", example="UrbanUser")],
+    age: Annotated[int, Path(ge=18, le=120, description="Enter age", example=24)]
+) -> str:
     if user_id in users:
         users[user_id] = f"Имя: {username}, возраст: {age}"
         return f"The user {user_id} is updated"
     return f"User {user_id} not found"
 
 @app.delete("/user/{user_id}")
-async def delete_user(user_id: str) -> str:
+async def delete_user(
+    user_id: Annotated[str, Path(description="Enter User ID")]
+) -> str:
     if user_id in users:
         users.pop(user_id)
         return f"User {user_id} has been deleted"
     return f"User {user_id} not found"
+
--- module_16_4.py
+++ module_16_4.py
@@ -4,25 +4,25 @@ from typing import List
 
 app = FastAPI()
 
+users = []
+
 class User(BaseModel):
     id: int
     username: str
     age: int
-
-users: List[User] = []
 
 @app.get("/users", response_model=List[User])
 async def get_users() -> List[User]:
     return users
 
 @app.post("/user/{username}/{age}", response_model=User)
-async def create_user(username: str, age: int) -> User:
-    user_id = (users[-1].id + 1) if users else 1
-    new_user = User(id=user_id, username=username, age=age)
+async def post_user(username: str, age: int) -> User:
+    new_id = 1 if not users else users[-1].id + 1
+    new_user = User(id=new_id, username=username, age=age)
     users.append(new_user)
     return new_user
 
-@app.put("/user/{user_id}", response_model=User)
+@app.put("/user/{user_id}/{username}/{age}", response_model=User)
 async def update_user(user_id: int, username: str, age: int) -> User:
     for user in users:
         if user.id == user_id:
@@ -38,3 +38,4 @@ async def delete_user(user_id: int) -> User:
             users.remove(user)
             return user
     raise HTTPException(status_code=404, detail="User was not found")
+
