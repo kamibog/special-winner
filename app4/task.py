3442ce7d88b0d8869c466358d35f96464c74b7e6
kamibog <kameneckaya@mail.ru>
2025-01-16 15:40:09 +0700

    Add files via upload

--- app4/task.py
+++ app4/task.py
@@ -1,23 +1,21 @@
-from fastapi import APIRouter
+from sqlalchemy import Column, Integer, String, Boolean, ForeignKey, Float
+from sqlalchemy.orm import relationship
+from app.backend.db import Base
+from app.models.user import User
 
-router = APIRouter(prefix="/task", tags=["task"])
+class Task(Base):
+    __tablename__ = 'tasks'
+    __table_args__ = {'extend_existing':True}
 
-@router.get("/")
-async def all_tasks():
-    pass
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String, nullable=False)
+    content = Column(String, nullable=True)
+    priority = Column(Integer, default=0)
+    completed = Column(Boolean, default=False)
+    user_id = Column(Integer, ForeignKey('users.id'), nullable=False, index=True)
+    slug = Column(String, unique=True, index=True)
 
-@router.get("/{task_id}")
-async def task_by_id(task_id: int):
-    pass
+    user = relationship("User", back_populates="tasks")
 
-@router.post("/create")
-async def create_task():
-    pass
-
-@router.put("/update")
-async def update_task():
-    pass
-
-@router.delete("/delete")
-async def delete_task():
-    pass
+from sqlalchemy.schema import CreateTable
+print(CreateTable(Task.__table__))
--- app4/user.py
+++ app4/user.py
@@ -1,23 +1,55 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends, status, HTTPException
+from sqlalchemy.orm import Session
+from backend.db_depends import get_db
+from typing import List
+from models import User
+from schemas import CreateUser, UpdateUser
+from sqlalchemy import insert, select, update, delete
+from slugify import slugify
 
-router = APIRouter(prefix="/user", tags=["user"])
+router = APIRouter(prefix='/user', tags=['user'])
 
-@router.get("/")
-async def all_users():
-    pass
 
-@router.get("/{user_id}")
-async def user_by_id(user_id: int):
-    pass
+@router.get('/', response_model=List[User])
+async def all_users(db: Annotated[Session, Depends(get_db)]) -> List[User]:
+    users = db.execute(select(User)).scalars().all()
+    return users
 
-@router.post("/create")
-async def create_user():
-    pass
 
-@router.put("/update")
-async def update_user():
-    pass
+@router.get('/{user_id}', response_model=User)
+async def user_by_id(user_id: int, db: Annotated[Session, Depends(get_db)]) -> User:
+    user = db.execute(select(User).where(User.id == user_id)).scalar_one_or_none()
+    if user is None:
+        raise HTTPException(status_code=404, detail='User was not found')
+    return user
 
-@router.delete("/delete")
-async def delete_user():
-    pass
+
+@router.post('/create', status_code=status.HTTP_201_CREATED)
+async def create_user(user: CreateUser, db: Annotated[Session, Depends(get_db)]):
+    user_slug = slugify(user.username)
+    new_user = User(**user.dict(), slug=user_slug)
+    db.execute(insert(User).values(new_user))
+    db.commit()
+    return {'status_code': status.HTTP_201_CREATED, 'transaction': 'Successful'}
+
+
+@router.put('/update/{user_id}', status_code=status.HTTP_200_OK)
+async def update_user(user_id: int, user: UpdateUser, db: Annotated[Session, Depends(get_db)]):
+    existing_user = db.execute(select(User).where(User.id == user_id)).scalar_one_or_none()
+    if existing_user is None:
+        raise HTTPException(status_code=404, detail='User was not found')
+
+    db.execute(update(User).where(User.id == user_id).values(**user.dict()))
+    db.commit()
+    return {'status_code': status.HTTP_200_OK, 'transaction': 'User update is successful!'}
+
+
+@router.delete('/delete/{user_id}', status_code=status.HTTP_204_NO_CONTENT)
+async def delete_user(user_id: int, db: Annotated[Session, Depends(get_db)]):
+    existing_user = db.execute(select(User).where(User.id == user_id)).scalar_one_or_none()
+    if existing_user is None:
+        raise HTTPException(status_code=404, detail='User was not found')
+
+    db.execute(delete(User).where(User.id == user_id))
+    db.commit()
+    return {'status_code': status.HTTP_204_NO_CONTENT, 'transaction': 'User deleted successfully!'}
